9ba3e39ff15202daa829105e3a35766db642ae4a
jacobiwoop <jacobiwoop@gmail.com>
2026-03-08 18:03:06 +0100

    modified:   basic-instinct-api/log.txt 	modified:   basic/src/pages/ExplorePage.tsx

--- basic/src/pages/ExplorePage.tsx
+++ basic/src/pages/ExplorePage.tsx
@@ -79,7 +79,7 @@ export const ExplorePage = () => {
         </div>
 
         {loading ? (
-          <div className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ${isChatOpen ? 'xl:grid-cols-4' : 'xl:grid-cols-5'} gap-4`}>
+          <div className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ${isChatOpen ? 'xl:grid-cols-3' : 'xl:grid-cols-5'} gap-4`}>
             {Array.from({ length: 15 }).map((_, index) => (
               <SkeletonCard key={`skeleton-${index}`} variant="explore" />
             ))}
@@ -87,7 +87,7 @@ export const ExplorePage = () => {
         ) : (
           <>
             {/* Grille de résultats */}
-            <div className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ${isChatOpen ? 'xl:grid-cols-4' : 'xl:grid-cols-5'} gap-4`}>
+            <div className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ${isChatOpen ? 'xl:grid-cols-3' : 'xl:grid-cols-5'} gap-4`}>
               {filteredCreators.map(creator => (
                 <div 
                   key={creator.id} 
